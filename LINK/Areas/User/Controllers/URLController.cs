84fdf45e0f6e46f61691edcb86cc544cc0e9b60c
kaleemwaheed <kingkaleem43@gmail.com>
2016-08-09 13:11:57 +0500

    URL controller add and form

---
+++ LINK/Areas/User/Controllers/URLController.cs
@@ -0,0 +1,32 @@
+﻿using BOL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LINK.Areas.User.Controllers
+{
+    public class URLController : Controller
+    {
+        // GET: User/URL
+        public ActionResult Index()
+        {
+            LinkHubDbEntities db = new LinkHubDbEntities();
+            ViewBag.CategoryId = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");
+
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(tbl_Url objurl)
+        {
+
+            //LinkHubDbEntities db = new LinkHubDbEntities();
+            //ViewBag.CategoryId = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");
+
+
+            return View();
+        }
+    }
+}
