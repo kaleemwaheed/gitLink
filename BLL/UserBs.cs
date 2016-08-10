3f496bc2bcf0321ba0ecd31da28bb3a0e918a3ef
kaleemwaheed <kingkaleem43@gmail.com>
2016-08-10 20:46:59 +0500

    BOL and DAL User and Category add Entity framwork

---
+++ BLL/UserBs.cs
@@ -0,0 +1,40 @@
+﻿using DAL;
+using BOL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class UserBs
+    {
+        private UserDb objDB;
+        public UserBs()
+        {
+            objDB = new UserDb();
+        }
+        public IEnumerable<tbl_User> GetAll()
+        {
+            return objDB.GetAll();
+        }
+        public tbl_User GetByID(int Id)
+        {
+            return objDB.GetByID(Id);
+        }
+        public void Insert(tbl_User user)
+        {
+            objDB.Insert(user);
+        }
+
+        public void Delete(int Id)
+        {
+            objDB.Delete(Id);
+        }
+        public void Update(tbl_User user)
+        {
+            objDB.Update(user);
+        }
+    }
+}
--- LINK/Areas/Common/Controllers/BrowseURLController.cs
+++ LINK/Areas/Common/Controllers/BrowseURLController.cs
@@ -17,23 +17,84 @@ namespace LINK.Areas.Common.Controllers
         }
 
         // GET: Common/BrowseURL
-        public ActionResult Index(String SortOrder,String SortBy)
+        public ActionResult Index(String SortOrder,String OrderBy,String Page)
         {
             ViewBag.SortOrder = SortOrder;
-            ViewBag.SortBy = SortBy;
+            ViewBag.OrderBy = OrderBy;
+            
             var urls = objurlbs.GetALL().Where(x => x.IsApproved=="A").ToList();
-            switch (SortOrder)
+            switch (OrderBy)
             {
-                case "Asc":
+            case "Title":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            urls = urls.OrderBy(x => x.UrlTitle).ToList();
+                            break;
+                        case "Desc":
+                            urls = urls.OrderByDescending(x => x.UrlTitle).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+
+                    break;
+               case "Url":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            urls = urls.OrderBy(x => x.Url).ToList();
+                            break;
+                        case "Desc":
+                            urls = urls.OrderByDescending(x => x.Url).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+
+                    break;
+               case "UrlDesc":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            urls = urls.OrderBy(x => x.UrlDesc).ToList();
+                            break;
+                        case "Desc":
+                            urls = urls.OrderByDescending(x => x.UrlDesc).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+                case "CategoryNAme":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            urls = urls.OrderBy(x => x.tbl_Category.CategoryName).ToList();
+                            break;
+                        case "Desc":
+                            urls = urls.OrderByDescending(x => x.tbl_Category.CategoryName).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+                default:
                     urls = urls.OrderBy(x => x.UrlTitle).ToList();
                     break;
-                case "Desc":
-                    urls = urls.OrderByDescending(x => x.UrlTitle).ToList();
-                    break;
-                default:
-                    break;
-                    
-            }//
+
+            }
+
+            ViewBag.TotalPages = Math.Ceiling(objurlbs.GetALL().Where(x => x.IsApproved == "A").Count() / 10.0);
+
+
+            int page = int.Parse(Page == null ? "1" : Page);
+            ViewBag.Page = page;
+            urls = urls.Skip((page - 1) * 10).Take(10).ToList();
+
+
             return View(urls);
         }
     }
